d6ec12343d155f7a9550ca2f97aa905fddd54e38
Tonyttyang <44243221+Tonyttyang@users.noreply.github.com>
2022-01-17 15:49:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
+
+
+morgan.token("data",function getBody(req){
+    return JSON.stringify(req.body) 
+})
+
+app.use(morgan(":method :url :status :res[content-length] -:response-time ms :data"))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    { 
+        "id": 5,
+        "name": "TT6", 
+        "number": "3123"
+    }
+]
+
+
+
+// generated random ID for phonebook
+const generateId = (max) =>{
+    return Math.floor(Math.random() * max);
+}
+
+
+// render webpage for localhost at port 3001
+app.get('/',(req,res)=>{
+    res.send("<div>hello 11123</div>")
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+app.get('/api/info', (req, res) => {
+    const len = persons.length
+    const date = new Date();
+    res.send(`Phonebook has info for ${len} people <br/> ${date}`)
+})    
+
+app.get('/api/persons/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+        res.json(person)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+  
+app.post('/api/persons',(req,res)=>{
+    const body = req.body
+    const nameFinder = persons.find(person => person.name === body.name)
+
+    if(!body.name){
+        res.status(404).json({
+            error: "name must be unique"
+        })
+    }
+    else if(!body.number){
+        res.status(404).json({
+            error: "number must be given"
+        })    
+    }
+    else if (nameFinder !== undefined){
+
+        res.status(404).json({
+            error: "name already exists in the phonebook"
+        })    
+        
+    }
+    else{
+        const person = {
+            name : body.name,
+            number: body.number,
+            id: generateId(1001)
+        }
+        persons = persons.concat(person)
+        res.json(persons)
+    }
+    
+   
+})
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT)
+console.log(`server runing on port ${PORT} `)
+
